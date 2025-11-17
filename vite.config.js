3c5a13b264a655f0c2be31fc38be7cce1fdfe986
HIMANSHU DIWAKAR THAKRE <himanshuthakre7509@gmail.com>
2025-11-17 10:35:05 +0530

    26

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  css: {
+    transformer: 'lightningcss',
+  },
+  build: {
+    cssMinify: 'lightningcss'
+  },
+  resolve: {
+    alias: {
+      // Force Vite to use the same CSS processor across platforms
+      'lightningcss': 'lightningcss'
+    }
+  }
 })
