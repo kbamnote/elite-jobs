fbd5678284ee1d758e0486d50601292c9211cb00
kbamnote3 <kunalbamnote7@gmail.com>
2025-11-18 15:01:46 +0530

    ch33

--- vite.config.js
+++ vite.config.js
@@ -33,9 +33,5 @@ export default defineConfig({
     hmr: {
       overlay: false
     }
-  },
-  // Add this to handle the rollup issue on Vercel
-  optimizeDeps: {
-    exclude: ['@rollup/rollup-linux-x64-gnu']
   }
 })
